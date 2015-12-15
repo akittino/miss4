98386ae54477f3da876d6182bcb02d16320830a1
Aleksander Kantak <kanti1992@gmail.com>
2015-12-16 00:41:29 +0100

    basic plot on hardcoded data

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+void plotChart()
+{
+  system("gnuplot < i");
+}
+
+double fun()
+{
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  plotChart();
+}
